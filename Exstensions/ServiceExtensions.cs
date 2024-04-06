b725cb9e0c4f436b74c2220fe840cc04dcb03746
dadaPipes <markoliverstockflethstaf@gmail.com>
2024-04-06 18:55:09 +0200

    chore: Add CORS

---
+++ Exstensions/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace LiftCommander_API.Exstensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                builder
+                .AllowAnyOrigin()   // more restrict example: WithOrigins("https://example.com") which will allow requests only from that concrete source.
+                .AllowAnyMethod()   // more restrict example: WithMethods("POST", "GET") that will allow only specific HTTP methods.
+                .AllowAnyHeader()); // more restrict example: WithHeaders("accept", "content type") method to allow only specific headers.
+            });
+
+
+    }
+}
